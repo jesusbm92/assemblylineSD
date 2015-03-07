0958899f2f9c4c37602aa8e915af8a427d6ee7e4
Hasser <Hasser@JAMES>
2015-03-07 02:04:52 +0100

    Se agrega la clase Engine

---
+++ AssemblyLine/src/domain/Engine.java
@@ -0,0 +1,27 @@
+package domain;
+
+public class Engine implements Command{
+	private Boolean isPlaced;
+	
+	public Engine(Boolean isPlaced) {
+		// TODO Auto-generated constructor stub
+		super();
+		this.isPlaced = isPlaced;
+	}
+	public Boolean getIsPlaced() {
+		return isPlaced;
+	}
+
+	public void setIsPlaced(Boolean isPlaced) {
+		this.isPlaced = isPlaced;
+	}
+
+	@Override
+	public void setup(Object AssemblyLineItem) {
+		Car c = (Car) AssemblyLineItem;
+		c.getEngine().setIsPlaced(true);
+		// To change for graphic application instead of console output
+		System.out.println("Engine placed correctly");
+	}
+
+}
