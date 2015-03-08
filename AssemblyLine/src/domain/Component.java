509001870d29b61d73ec20b8c3a560945ebce6fd
Jesus Barba <jesusbm92@gmail.com>
2015-03-08 13:43:03 +0100

    Added Component class to commit

---
+++ AssemblyLine/src/domain/Component.java
@@ -0,0 +1,20 @@
+package domain;
+
+public class Component {
+
+	private Boolean isPlaced;
+
+	public Component() {
+		super();
+		this.isPlaced = false;
+	}
+
+	public Boolean getIsPlaced() {
+		return isPlaced;
+	}
+
+	public void setIsPlaced(Boolean isPlaced) {
+		this.isPlaced = isPlaced;
+	}
+
+}
