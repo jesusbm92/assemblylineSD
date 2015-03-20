ab8e29e59f0fe908cbfa0021984b7943cc2b5ffd
Pablo Nieto <pablo.nietomaestre@gmail.com>
2015-03-20 18:19:43 +0100

    First version of Component.java

--- AssemblyLine/src/domain/Component.java
+++ AssemblyLine/src/domain/Component.java
@@ -3,10 +3,12 @@ package domain;
 public class Component {
 
 	private Boolean isPlaced;
+	private int stock;
 
 	public Component() {
 		super();
 		this.isPlaced = false;
+		this.stock = 0;
 	}
 
 	public Boolean getIsPlaced() {
@@ -15,6 +17,14 @@ public class Component {
 
 	public void setIsPlaced(Boolean isPlaced) {
 		this.isPlaced = isPlaced;
+	}
+	
+	public void setStock(int n){
+		this.stock = n;
+	}
+	
+	public int getStock(){
+		return stock;
 	}
 
 }
