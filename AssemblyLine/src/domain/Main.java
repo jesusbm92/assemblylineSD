1180bb1aaf6054934e4276b37fa51f78ea956a54
mfritzsche <martin.fritzsche@citrix.com>
2015-03-22 20:11:18 +0100

    fixed the two circle stations to be different objects, thus showing different stock amounts

--- AssemblyLine/src/domain/Main.java
+++ AssemblyLine/src/domain/Main.java
@@ -5,11 +5,12 @@ public class Main {
 	public static void main(String[] args) {
 
 		AssemblyLine al = new AssemblyLine();
-		Circle circle = new Circle();
+		Circle circle1 = new Circle();
+		Circle circle2 = new Circle();
 		Rectangle rect = new Rectangle();
-		AssemblyStation as1 = new AssemblyStation(circle);
+		AssemblyStation as1 = new AssemblyStation(circle1);
 		AssemblyStation as2 = new AssemblyStation(rect);
-		AssemblyStation as3 = new AssemblyStation(circle);
+		AssemblyStation as3 = new AssemblyStation(circle2);
 		Warehouse wh = new Warehouse();
 		as1.setWarehouse(wh);
 		as2.setWarehouse(wh);
