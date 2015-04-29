7236198d554fc088bbcabec9e25b88f6eaefa2aa
xincruisewang <xincruisewang@gmail.com>
2015-04-29 21:25:56 +0400

    oo

--- AssemblyLine/src/domain/Circle.java
+++ AssemblyLine/src/domain/Circle.java
@@ -1,5 +1,3 @@
-package domain;
-
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
@@ -11,8 +9,7 @@ public class Circle extends Figure {
 	public BufferedImage draw(int width, int height) {
 		BufferedImage img = new BufferedImage(width,height,BufferedImage.TYPE_INT_RGB);
         Graphics2D g = img.createGraphics();
-        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
-        g.setColor(Color.GREEN);
+        g.setColor(Color.RED);
         g.fillOval(0, 0, width, height);
 		return img;
 	}
