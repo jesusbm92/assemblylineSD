f002000d1fd040f1027235210e043c619445425e
Hasser <Hasser@JAMES>
2015-04-30 22:02:58 +0200

    AssemblyStation Updated. Composed by a Figure. Draw Method Updated.

--- AssemblyLine/src/domain/AssemblyStation.java
+++ AssemblyLine/src/domain/AssemblyStation.java
@@ -1,19 +1,25 @@
 package domain;
 
+import java.awt.Color;
 import java.awt.FontMetrics;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 
-public class AssemblyStation implements Drawable {
+public class AssemblyStation  {
 	private Component component;
 	private Warehouse warehouse;
+	private Figure F;
 
 	// private int StockInStation;
 
 	public AssemblyStation(Component c) {
 		component = c;
+		F= new Rectangle();
+		
 	}
-
+	public void AddComponent(Component c) {
+		component = c;
+	}
 	public Boolean placeComponent(Product p) {
 		if (component.getStock() < 1) {
 			warehouse.generateComponents(component, 1);
@@ -27,19 +33,26 @@ public class AssemblyStation implements Drawable {
 
 	}
 
-	@Override
+	
 	public BufferedImage draw(int width, int height) {
+		
+
 		BufferedImage img = new BufferedImage(width, height,
 				BufferedImage.TYPE_INT_RGB);
 		Graphics2D graphics = img.createGraphics();
 
 		FontMetrics metrics = graphics.getFontMetrics(graphics.getFont());
 		
-		graphics.drawRect(0, 0, width - 1, height - 1);
+		//graphics.drawRect(0, 0, width - 1, height - 1);
+		
+		
+		graphics.drawImage(F.draw(width - 1, height - 1),null,0,0);
+				
+				
 		graphics.drawString("Component:  ", 5, height/3);
-		graphics.drawImage(component.draw(metrics.getHeight() - 1, metrics.getHeight() - 1), null,
+		graphics.drawImage(SimpleComponent.draw(metrics.getHeight() - 1, metrics.getHeight() - 1), null,
 				5 + metrics.stringWidth("Component:  "), height/3 - metrics.getHeight() + 1);
-		graphics.drawString("Stock:  " + String.valueOf(component.getStock()), 5, height/3 * 2);
+		graphics.drawString("Stock:  " + String.valueOf(SimpleComponent.getStock()), 5, height/3 * 2);
 		
 		return img;
 	}
