cc877bcd9257f8f8b74504707ae1e9bdb6d2b3eb
mfritzsche <martin.fritzsche@citrix.com>
2015-05-19 19:55:21 +0200

    remove getters and setters from figure

--- AssemblyLine/src/domain/Figure.java
+++ AssemblyLine/src/domain/Figure.java
@@ -14,31 +14,6 @@ abstract class Figure {
 		this.color = color;
 	}
 
-	public int getWidth() {
-		return width;
-	}
-
-	public void setWidth(int width) {
-		this.width = width;
-	}
-
-	public int getHeight() {
-		return height;
-	}
-
-	public void setHeight(int height) {
-		this.height = height;
-	}
-
-	public Color getColor() {
-		return color;
-	}
-
-	public void setColor(Color color) {
-		this.color = color;
-	}
-	
-
 	public abstract BufferedImage draw();
 
 }
